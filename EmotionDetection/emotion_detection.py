5fcb9f36f5b3140126cbb028b32c5a7f13572da7
Alon Tavori <alon.tavori@gmail.com>
2026-06-11 10:11:21 -0400

    Add error handling and update server

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,12 +6,24 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} # Set the headers required for the API request 
     response = requests.post(url, json = myobj, headers=header) # Send a POST request to the API with the text and headers 
     
-    #Parsing the JSON response from the API 
-    formatted_response = json.loads(response.text)
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
 
-    # Extracting emotions label and score from the response 
+    formatted_response = json.loads(response.text)
     emotions = formatted_response['emotionPredictions'][0]['emotion']
 
-    # Returning a dictionary containing sentiment analysis results 
-    return {'anger': emotions['anger'], 'disgust': emotions['disgust'],'fear': emotions['fear'], 'joy': emotions['joy'], 'sadness': emotions['sadness'], 'dominant_emotion': max(emotions, key=emotions.get)}
-
+    return {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': max(emotions, key=emotions.get)
+    }
